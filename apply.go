e6c9b517a32cb523300e8e34c27bba0e26b936cc
GrusnyDance <grusny.dance@gmail.com>
2023-04-10 02:07:23 +0200

    all in

--- apply.go
+++ apply.go
@@ -1,5 +1,24 @@
 package validator
 
-func apply() {
+import "errors"
 
+func Apply(title, text string) error {
+	if title == "" {
+		return errors.New("empty title")
+	}
+	if len(title) >= 100 {
+		return errors.New("title too long")
+	}
+	if text == "" {
+		return errors.New("empty text")
+	}
+	if len(text) >= 500 {
+		return errors.New("text too long")
+	}
+	return nil
 }
+
+//Название не должно быть пустым.
+//Название должно быть короче 100 символов.
+//Текст объявления не должен быть пустым.
+//Текст объявления должен быть короче 500 символов.
